c388e470d1e45a2e4fa4076e7856a0d9e573a833
Lakshmi P Mantravadi <prasanthiml@gmail.com>
2025-06-17 13:29:52 -0500

    updated vite config file: base setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'Accessible-ToDo',
+  base: '/Accessible-ToDo/',
   plugins: [react(), tailwindcss()],
 })
